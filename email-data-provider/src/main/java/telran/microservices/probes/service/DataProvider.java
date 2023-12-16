15dd9d3f24303a77939adb8480844c40625eedb0
Alex Pushilin <aepushilin@gmail.com>
2023-12-16 20:02:32 +0200

    Adds data-provider impl

---
+++ email-data-provider/src/main/java/telran/microservices/probes/service/DataProvider.java
@@ -0,0 +1,9 @@
+package telran.microservices.probes.service;
+
+
+import telran.microservices.probes.dto.EmailData;
+
+public interface DataProvider {
+
+    EmailData getEmailData(long sensorId);
+}
